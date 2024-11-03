c06a943082109f770d2be847bdd0696b207f4bb1
dlin1765 <dannylin1765@gmail.com>
2024-11-02 18:11:19 -0700

    reorganized files, started work on next model iteration, making data set smaller and adding more components

--- specific-moves-factory.py
+++ specific-moves-factory.py
@@ -6,7 +6,7 @@ import re
 @Language.component("specificMoves")
 def specificMoves(doc):
     new_ents = []
-    expression = "(?:(?:[udbnf]{0,10}|([udbnf]+[\s,.+]*){0,10}|(sway)?|(dpd)?|(ss)?)?[\s,]*[1-4](?:\s*[,+-]\s*[1-4])*)+|((?:[1-4](?:\s*[,+-]\s*[1-4])*)+)"
+    expression = "(?:(?:[udbnf]{0,10}|([udbnf]+[\s,.+]*){0,10}|(sway)?|(dpd)?|(ss)?|(ws)?|(fc)?|(wr)?)?[\s,]*[1-4](?:\s*[,+-]\s*[1-4])*)+|((?:[1-4](?:\s*[,+-]\s*[1-4])*)+)"
     for match in re.finditer(expression, doc.text):
         start, end = match.span()
         span = doc.char_span(start, end, label = "SPECIFIC_MOVE")
@@ -22,7 +22,8 @@ def tekkenSpecific(doc):
     new_ents = []
     matcher = PhraseMatcher(nlp.vocab, attr = matcher_attr)
     props = ['high crushes', 'high crushing', 'high crush','low crush', 'low crushing', 'low crushes', 'guard break', 'grounded', 'guard crush', 'guard crushes', 'guard crushing','flip over', 'in crouch',  'chargable', 'holdable', 'blue spark', 'blue sparks', 'knocks down']
-    moves = ['heat burst', 'heat smash', 'chain throw', 'chain grab', 'power crush','homing', 'armor', 'armored', 'parry', 'sabaki', 'instant screw', 'instant tornado', 'rage art']
+    moves = ['heat burst', 'heat smash', 'chain throw', 'chain grab', 'power crush','homing', 'armor', 'armored', 'parry', 'sabaki', 'instant screw', 'instant tornado', 'rage art', 'heat engager', 'heat engages', 'heat engaging', 'stance', 'transition', 'transitions']
+    stances = []
     patterns = [nlp.make_doc(text) for text in props]
     movePatterns = [nlp.make_doc(text) for text in moves]
     matcher.add("PROPERTIES", patterns)
